341bdf7b95a594c81a84e9bd5dc238daabf78016
Tom Wieschalla <tom.wieschalla@adesso.de>
2016-12-20 22:55:48 +0100

    finished gps and gyro view

---
+++ pages/Gyro/script.js
@@ -0,0 +1,19 @@
+function init() {
+    var x,y,z;
+
+    if (window.DeviceMotionEvent) {
+        window.addEventListener('devicemotion', function(event) {
+           x = event.rotationRate.beta;
+           y = event.rotationRate.gamma;
+           z = event.rotationRate.alpha;
+        });
+    }
+
+    window.setInterval(setData, 500);
+
+    function setData() {
+        $('#result1').html(x/57.2958);
+        $('#result2').html(y/57.2958);
+        $('#result3').html(z/57.2958);
+    }
+}
